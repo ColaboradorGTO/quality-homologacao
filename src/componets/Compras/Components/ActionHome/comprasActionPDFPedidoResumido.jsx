5671302603a836ec001248b995edc46c93c129ea
Myltiane Alves <myltiane.aux@gmail.com>
2026-03-18 08:56:47 -0300

    refactor: compras ActionPDFPedidoResumido

--- src/componets/Compras/Components/ActionHome/comprasActionPDFPedidoResumido.jsx
+++ src/componets/Compras/Components/ActionHome/comprasActionPDFPedidoResumido.jsx
@@ -11,67 +11,76 @@ import * as XLSX from 'xlsx';
 import HeaderTable from "../../../Tables/headerTable";
 
 export const ActionPDFPedidoResumido = ({ dadosPedidos }) => {
-    const [globalFilterValue, setGlobalFilterValue] = useState('');
-    const [rowSelection, setRowSelection] = useState(null);
-    const dataTableRef = useRef();
-  
-    const onGlobalFilterChange = (e) => {
-      setGlobalFilterValue(e.target.value);
-    };
-  
-    const handlePrint = useReactToPrint({
-      content: () => dataTableRef.current,
-      documentTitle: 'Relacao Detalhe Pedidos',
-    });
-  
-    const exportToPDF = () => {
-      const doc = new jsPDF();
-      doc.autoTable({
-        head: [['Data', 'Nº Pedido', 'Marca', 'Comprador', 'Fornecedor', 'QTD Produto', 'Vr Compra', 'Vr Venda', 'Vr Lucro', '(%) Lucro', 'Setor']],
-        body: dados.map(item => [
+  const [globalFilterValue, setGlobalFilterValue] = useState('');
+  const [rowSelection, setRowSelection] = useState(null);
+  const dataTableRef = useRef();
+
+  const onGlobalFilterChange = (e) => {
+    setGlobalFilterValue(e.target.value);
+  };
+
+  const handlePrint = useReactToPrint({
+    content: () => dataTableRef.current,
+    documentTitle: 'Relacao Detalhe Pedidos',
+  });
+
+  const exportToPDF = () => {
+    const doc = new jsPDF();
+    doc.autoTable({
+      head: [['Data', 'Nº Pedido', 'Marca', 'Comprador', 'Fornecedor', 'Valor Pedido', 'Setor', 'Status']],
+      body: dados.map(item => {
+        let setorTexto = item.DSSETOR === 'COMPRASADM' ? 'COMPRAS ADM' : item.DSSETOR;
+        let statusTexto = item.DSSETOR === 'CADASTRO' ? '' : item.DSANDAMENTO;
+        return [
           item.DTPEDIDO,
           item.IDPEDIDO,
-          item.NOFANTASIAGRUPO,
+          item.NOFANTASIA,
           item.NOMECOMPRADOR,
-          item.NOFANTASIAFORN,
-          formatMoeda(item.VRTOTALCUSTO),
-          formatMoeda(item.VRTOTALVENDA),
-          formatMoeda(item.VRTOTALLUCRO),
-          item.totalValorPercentualLucro,
-          item.DSSETOR == 'CADASTRO' ? 'CADASTRO' : item.DSSETOR == 'COMPRAS' ? 'COMPRAS' : item.DSSETOR == 'COMPRAS ADM' ? 'COMPRAS ADM' : '',
-        ]),
-        horizontalPageBreak: true,
-        horizontalPageBreakBehaviour: 'immediately'
-      });
-      doc.save('relacao_det_pedidos.pdf');
-    };
-  
-    const exportToExcel = () => {
-      const worksheet = XLSX.utils.json_to_sheet(dados);
-      const workbook = XLSX.utils.book_new();
-      const header = ['Data', 'Nº Pedido', 'Marca', 'Comprador', 'Fornecedor', 'QTD Produto', 'Vr Compra', 'Vr Venda', 'Vr Lucro', '(%) Lucro', 'Setor'];
-      worksheet['!cols'] = [
-        { wpx: 150, caption: 'Data' },
-        { wpx: 100, caption: 'Nº Pedido' },
-        { wpx: 150, caption: 'Marca' },
-        { wpx: 200, caption: 'Comprador' },
-        { wpx: 200, caption: 'Fornecedor' },
-        { wpx: 100, caption: 'QTD Produto' },
-        { wpx: 100, caption: 'Vr Compra' },
-        { wpx: 100, caption: 'Vr Venda' },
-        { wpx: 100, caption: 'Vr Lucro' },
-        { wpx: 100, caption: '(%) Lucro' },
-        { wpx: 100, caption: 'Setor' },
-      ];
-      XLSX.utils.sheet_add_aoa(worksheet, [header], { origin: 'A1' });
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Relacao Detalhe Pedidos');
-      XLSX.writeFile(workbook, 'relacao_det_pedidos.xlsx');
-    };
-  
+          item.NOFORNECEDOR,
+          formatMoeda(item.VRTOTALLIQUIDO),
+          setorTexto,
+          statusTexto,
+        ];
+      }),
+      horizontalPageBreak: true,
+      horizontalPageBreakBehaviour: 'immediately'
+    });
+    doc.save('relacao_det_pedidos.pdf');
+  };
+
+  const exportToExcel = () => {
+    const dadosProcessados = dados.map(item => ({
+      Data: item.DTPEDIDO,
+      'Nº Pedido': item.IDPEDIDO,
+      Marca: item.NOFANTASIA,
+      Comprador: item.NOMECOMPRADOR,
+      Fornecedor: item.NOFORNECEDOR,
+      'Valor Pedido': formatMoeda(item.VRTOTALLIQUIDO),
+      Setor: item.DSSETOR === 'COMPRASADM' ? 'COMPRAS ADM' : item.DSSETOR,
+      Status: item.DSSETOR === 'CADASTRO' ? '' : item.DSANDAMENTO
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(dadosProcessados);
+    const workbook = XLSX.utils.book_new();
+    const header = ['Data', 'Nº Pedido', 'Marca', 'Comprador', 'Fornecedor', 'Valor Pedido', 'Setor', 'Status'];
+    worksheet['!cols'] = [
+      { wpx: 150, caption: 'Data' },
+      { wpx: 100, caption: 'Nº Pedido' },
+      { wpx: 150, caption: 'Marca' },
+      { wpx: 200, caption: 'Comprador' },
+      { wpx: 200, caption: 'Fornecedor' },
+      { wpx: 100, caption: 'Valor Pedido' },
+      { wpx: 100, caption: 'Setor' },
+      { wpx: 100, caption: 'Status' },
+    ];
+    XLSX.utils.sheet_add_aoa(worksheet, [header], { origin: 'A1' });
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Relacao Pedidos Resumido');
+    XLSX.writeFile(workbook, 'relacao_pedidos_resumido.xlsx');
+  };
+
 
   const dados = dadosPedidos.map((item, index) => {
     let contador = index + 1;
-    // console.log(contador.length, 'contador')
     return {
       IDPEDIDO: item.IDPEDIDO,
       DTPEDIDO: item.DTPEDIDO,
@@ -106,11 +115,6 @@ export const ActionPDFPedidoResumido = ({ dadosPedidos }) => {
 
   const [totalQuantidadePedidos, setTotalQuantidadePedidos] = useState(0);
   const colunasPedidoResumido = [
-    // {
-    //   field: 'contador',
-    //   header: 'Nº',
-    //   body: row => row.contador,
-    // },
     {
       field: 'DTPEDIDO',
       header: 'Data',
@@ -146,42 +150,43 @@ export const ActionPDFPedidoResumido = ({ dadosPedidos }) => {
       field: 'DSSETOR',
       header: 'Setor',
       body: (row) => {
+        let colorSetorPedido = 'red';
+        let setorTexto = row.DSSETOR;
+
         if (row.DSSETOR == 'COMPRAS') {
-          return (
-            <p style={{ color: 'blue' }} >COMPRAS</p>
-          )
-
+          colorSetorPedido = 'blue';
         } else if (row.DSSETOR == 'CADASTRO') {
-          return (
-            <p estyle={{ color: 'red' }} >CADASTRO</p>
-          )
-
+          colorSetorPedido = 'red';
         } else if (row.DSSETOR == 'COMPRASADM') {
-          return (
-            <p style={{ color: 'red' }}>COMPRAS ADM</p>
-          )
+          setorTexto = 'COMPRAS ADM';
+          colorSetorPedido = 'red';
         }
+        return (
+          <p style={{ color: colorSetorPedido, fontSize: '0.8rem', fontWeight: 'bold' }}>
+            {setorTexto}
+          </p>
+        );
       }
     },
     {
       field: 'DSANDAMENTO',
       header: 'Status',
       body: (row) => {
-        if (row.DSANDAMENTO == 'PEDIDO INICIADO') {
-          return (
-            <p style={{ color: 'blue' }} >PEDIDO INICIADO</p>
-          )
-
-        } else if (row.DSANDAMENTO == 'PEDIDO FINALIZADO') {
-          return (
-            <p estyle={{ color: 'tomato' }} >PEDIDO FINALIZADO</p>
-          )
-
-        } else if (row.DSANDAMENTO == 'PEDIDO CANCELADO') {
-          return (
-            <p style={{ color: 'red' }}>PEDIDO CANCELADO</p>
-          )
+        let colorStPedido = 'blue';
+        let stPedido = row.DSANDAMENTO;
+        if (row.DSSETOR == 'COMPRAS') {
+          colorStPedido = stPedido == 'PEDIDO FINALIZADO' ? 'tomato' :
+            stPedido == 'PEDIDO INICIADO' ? 'blue' : 'red';
+        } else if (row.DSSETOR == 'CADASTRO') {
+          stPedido = '';
+        } else if (row.DSSETOR == 'COMPRASADM') {
+          colorStPedido = 'green';
         }
+        return (
+          <p style={{ color: colorStPedido, fontSize: '0.8rem' }}>
+            {stPedido}
+          </p>
+        );
       }
     }
   ]
@@ -189,17 +194,17 @@ export const ActionPDFPedidoResumido = ({ dadosPedidos }) => {
     <Fragment>
       <div >
 
-        <div 
-          style={{ 
-            fontWeight: 700, 
-            fontSize: "16px", 
-            border: '1px solid #000', 
-            textAlign: 'center', 
-            marginBottom: '10px', 
-            marginTop: '10px' 
+        <div
+          style={{
+            fontWeight: 700,
+            fontSize: "16px",
+            border: '1px solid #000',
+            textAlign: 'center',
+            marginBottom: '10px',
+            marginTop: '10px'
           }}>
-            <h2>RELAÇÃO DE PEDIDOS RESUMIDO</h2>
-            
+          <h2>RELAÇÃO DE PEDIDOS RESUMIDO</h2>
+
         </div>
         <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
           <HeaderTable
@@ -240,29 +245,29 @@ export const ActionPDFPedidoResumido = ({ dadosPedidos }) => {
                 body={coluna.body}
                 footer={coluna.footer}
                 sortable={coluna.sortable}
-                headerStyle={{ color: '#FFF', backgroundColor: "#7a59ad", border: '1px solid #000', fontSize: '1rem', textAlign: 'center' }}
+                headerStyle={{ color: '#FFF', backgroundColor: "#7a59ad", border: '1px solid #000', fontSize: '0.8rem', textAlign: 'center' }}
                 footerStyle={{ color: '#212529', backgroundColor: "transparent", border: '1px solid #000', fontSize: '0.8rem' }}
-                bodyStyle={{ fontSize: '1rem', backgroundColor: 'transparent', border: '1px solid #000', textAlign: 'initial', fontWeight: 600 }}
+                bodyStyle={{ fontSize: '0.8rem', backgroundColor: 'transparent', border: '1px solid #000', textAlign: 'initial', fontWeight: 600 }}
 
               />
             ))}
           </DataTable>
 
           <div className="mt-6">
-              <table className="semborda">
-
+            <table className="semborda">
+              <tr>
                 {dados && dados.length > 0 && (
                   <>
                     <th style={{ textAlign: 'left', fontSize: '14px' }}>Quantidade de Pedidos: </th>
                     <th style={{ textAlign: 'right', fontSize: '14px' }}> <b>{calcularTotalContador()}</b></th>
                   </>
                 )}
-                <tr>
-
-                  <th style={{ textAlign: 'left', fontSize: '14px' }}>Total de Pedidos: </th>
-                  <th style={{ textAlign: 'right', fontSize: '14px' }}> <b>{formatMoeda(calcularTotalPedido())}</b></th>
-                </tr>
-              </table>
+              </tr>
+              <tr>
+                <th style={{ textAlign: 'left', fontSize: '14px' }}>Total de Pedidos: </th>
+                <th style={{ textAlign: 'right', fontSize: '14px' }}> <b>{formatMoeda(calcularTotalPedido())}</b></th>
+              </tr>
+            </table>
           </div>
           <div>
 
